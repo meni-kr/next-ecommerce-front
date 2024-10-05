5416c645ebd88b4a9d6ee7a0e1352d7400750047
meni-kr <meni.kramer@gmail.com>
2024-10-05 16:34:12 +0300

    Style featured component in home page

---
+++ components/Button.js
@@ -0,0 +1,43 @@
+import styled, { css } from "styled-components"
+
+
+const StyledButton = styled.button`
+    border:0;
+    padding: 5px 15px;
+    border-radius:5px;
+    cursor: pointer;
+    display: inline-flex;
+    align-items: center;
+    svg{
+       height: 16px;
+       margin-right: 5px; 
+    }
+    ${props => props.white === 'y' && props.outline == 'n' && css`
+        background-color: #fff;
+        color: #000;
+        `}
+    ${props => props.white === 'y' && props.outline == 'y' && css`
+        background-color: transparent;
+        color: #fff;
+        border: 1px solid #fff;
+        `}
+    ${props => props.primary === 'y' && css`
+        background-color: #5542F6;
+        border: 1px solid #5542F6;
+        color: #fff;
+        `}
+    ${props => props.size === 'l' && css`
+        font-size:1.2rem;
+        padding: 10px 20px;
+        svg{
+            height: 20px; 
+        }
+        `}
+`;
+
+export default function Button({ children, ...rest }) {
+
+    return (
+        <StyledButton {...rest}>{children}</StyledButton>
+    )
+}
