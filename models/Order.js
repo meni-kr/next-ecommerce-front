e436d8ae5a579f5975be3186ffb41b813ed7c9da
meni-kr <meni.kramer@gmail.com>
2024-10-07 16:50:47 +0300

    Add time stamp to order model

--- models/Order.js
+++ models/Order.js
@@ -8,6 +8,8 @@ const OrderSchema = new Schema({
     streetAddress:String,
     county:String,
     paid:Boolean,
+},{
+    timestamps:true,
 })
 
 export const Order = models?.Order || model('Order', OrderSchema)
