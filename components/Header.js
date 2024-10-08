f5df1ea399fbebd90441934879c44e79013e48d8
meni-kr <meni.kramer@gmail.com>
2024-10-08 15:48:08 +0300

    Make header responsive

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,19 @@
 import Link from "next/link";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import Center from "./Center";
 import { CartContext } from "./CartContext";
-import{useContext} from "react";
+import { useContext, useState } from "react";
+import BarsIcon from "./icons/Bars";
 
 const StyledHeader = styled.header`
-background-color: #222;
+    background-color: #222;
 `;
 
 const Logo = styled(Link)`
     color: #fff;
     text-decoration:none;
+    position: relative;
+    z-index:3;
 `;
 
 const Wrapper = styled.div`
@@ -20,31 +23,81 @@ const Wrapper = styled.div`
 `;
 
 const StyledNav = styled.nav`
-    display: flex;
+    display: none;
+    
+    ${props=>props.$mobileNavActive ==='open' ?`
+        background-color: white;
+            display:block;
+        `:`
+        display: none;
+        `
+    }
+    
     gap: 15px;
+    position: fixed;
+    top: 0px;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    padding: 70px 20px 20px;
+    background-color: #222;
+    @media screen and (min-width: 768px){
+        display: flex;
+        position: static;
+        padding: 0;
+        background-color: transparent;
+    }
 `;
 
 const NavLink = styled(Link)`
+    display: block;
     color: #aaa;
     text-decoration:none;
+    padding: 10px 0;
+    @media screen and (min-width: 768px){
+    padding: 0;}
+`;
 
+const NavButton = styled.button`
+    background-color: transparent;
+    width: 35px;
+    height: 35px;
+    border: 0;
+    color: white;
+    cursor: pointer;
+    position: relative;
+    z-index:3;
+    @media screen and (min-width: 768px){
+        display: none;
+    }
 `;
 
 export default function Header() {
-    const {cartProducts} = useContext(CartContext)
+    const { cartProducts } = useContext(CartContext)
+    const [mobileNavActive, setMobileNavActive] = useState('close')
+
+// console.log('mobileNavActive:', mobileNavActive?.toString())
+
+    function toggleNav(){
+        setMobileNavActive(mobileNavActive === 'close' ? 'open' : 'close')
+        console.log(mobileNavActive)
+    }
 
     return (
         <StyledHeader>
             <Center>
                 <Wrapper>
                     <Logo href={'/'}>Ecommerce</Logo>
-                    <StyledNav>
+                    <StyledNav $mobileNavActive={mobileNavActive}>
                         <NavLink href={'/'}>Home</NavLink>
                         <NavLink href={'/products'}>All products</NavLink>
                         <NavLink href={'/categories'}>Categories</NavLink>
                         <NavLink href={'/account'}>Account</NavLink>
                         <NavLink href={'/cart'}>Cart ({cartProducts.length})</NavLink>
                     </StyledNav>
+                    <NavButton onClick={toggleNav}>
+                        <BarsIcon />
+                    </NavButton>
                 </Wrapper>
             </Center>
         </StyledHeader>
